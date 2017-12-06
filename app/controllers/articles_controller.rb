53fc5cec4eac865100a0b171112d32b464874bd3
michalfirszt <michalfiszu@gmail.com>
2017-12-06 11:29:07 +0100

    authenticate user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /articles
   # GET /articles.json
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/new
   def new
-    @article = Article.new
+    @article = current_user.articles.build
   end
 
   # GET /articles/1/edit
@@ -24,7 +26,7 @@ class ArticlesController < ApplicationController
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
 
     respond_to do |format|
       if @article.save
@@ -71,4 +73,10 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:description)
     end
+
+    def correct_user
+      @article = current_user.articles.find_by(id: params[:id])
+      redirect_to articles_path, notice: "You are not allowed to edit this article" if @article.nil?
+    end
+
 end
