3c1a7502b9040210703347917678cd6b9b62f614
michalfirszt <michalfiszu@gmail.com>
2018-03-20 23:44:12 +0100

    remove image changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
